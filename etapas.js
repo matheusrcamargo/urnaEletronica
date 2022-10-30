4cee1c33873921c5be6e480264912b0618d50336
Matheus Camargo <matheusrcamargo2002@hotmail.com>
2022-10-30 14:04:55 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {   numero: '24242',
+                nome: 'Fulano',
+                partido: 'ABC',
+                fotos:[
+                    {url: '24242.png', legenda: 'Vereador', small: false}
+                ]
+
+            },
+            {
+                numero: '88888',
+                nome: 'Beltrano Camargo',
+                partido: 'CBA',
+                fotos:[
+                    {url:'88888.png', legenda: 'Vereador'}
+                ]
+            }
+
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos:[
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url: '99.jpg', legenda: 'Prefeito'},
+                    {url: '99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos:[
+                    {url: '84.jpg', legenda: 'Prefeito'},
+                    {url: '84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
